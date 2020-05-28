3f9982498047086814f409923659891ee2ca8718
denis.burtsev <denis.burtsev@thinfilmnfc.com>
2020-05-28 22:57:06 +0300

    lesson2 update

--- src/ru/geekbrains/lesson2/Lesson2.java
+++ src/ru/geekbrains/lesson2/Lesson2.java
@@ -17,7 +17,8 @@ public class Lesson2 {
         moveElements(randArr, 2);
 
     }
-
+//1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+// С помощью цикла и условия заменить 0 на 1, 1 на 0;
     private static void revertArrayValues(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 0) {
@@ -27,14 +28,14 @@ public class Lesson2 {
             }
         }
     }
-
+//2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
     private static void fillAnArray() {
         int[] arr = new int[8];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = i * 3;
         }
     }
-
+//3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
     private static void multiplyElements() {
         int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
@@ -43,7 +44,8 @@ public class Lesson2 {
             }
         }
     }
-
+//4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+// и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
     private static void fillDiagonals() {
         int[][] arr = new int[5][5];
         for (int i = 0; i < arr.length; i++) {
@@ -52,6 +54,8 @@ public class Lesson2 {
         }
     }
 
+
+//5. ** Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета);
     private static void minAndMax(int[] arr) {
         int minimum = arr[0];
         int maximum = arr[0];
@@ -63,6 +67,11 @@ public class Lesson2 {
             }
         }
     }
+
+//6. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
+//если в массиве есть место, в котором сумма левой и правой части массива равны.
+//Примеры: checkBalance([2, 2, 2, 1, 2, 2, || 10, 1]) → true,
+//checkBalance([1, 1, 1, || 2, 1]) → true, граница показана символами ||, эти символы в массив не входят.
 
     private static boolean checkBalance(int[] arr) {
         int arraySum = 0;
@@ -81,6 +90,8 @@ public class Lesson2 {
         return false;
     }
 
+//7. **** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным, или отрицательным),
+//при этом метод должен сместить все элементымассива на n позиций. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
     private static void moveElements(int[] arr, int n) {
         if (arr.length == 0 ||n % arr.length == 0) {
             return;
