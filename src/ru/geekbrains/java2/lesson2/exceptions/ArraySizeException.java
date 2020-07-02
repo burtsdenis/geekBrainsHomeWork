254c68ba6c4c1aa7266cf9c90067ee211c1195d3
denis.burtsev <denis.burtsev@thinfilmnfc.com>
2020-07-02 22:04:45 +0300

    change Exception class inheritance to RuntimeException

--- src/ru/geekbrains/java2/lesson2/exceptions/ArraySizeException.java
+++ src/ru/geekbrains/java2/lesson2/exceptions/ArraySizeException.java
@@ -1,6 +1,6 @@
 package ru.geekbrains.java2.lesson2.exceptions;
 
-public class ArraySizeException extends Exception {
+public class ArraySizeException extends RuntimeException {
     public ArraySizeException(String message) {
         super(message);
     }
