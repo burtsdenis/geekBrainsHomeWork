40e4be0a99caf416ba05d1eedfbe08376b937c18
denis.burtsev <denis.burtsev@thinfilmnfc.com>
2020-07-02 14:45:16 +0300

    lesson 2 homework

---
+++ src/ru/geekbrains/java2/lesson2/exceptions/ArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.java2.lesson2.exceptions;
+
+public class ArraySizeException extends Exception {
+    public ArraySizeException(String message) {
+        super(message);
+    }
+}
